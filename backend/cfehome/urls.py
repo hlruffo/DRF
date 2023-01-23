a781ef23c94ef393168f7eab0f54622a6be2b1bd
Hugo Ledertheil Ruffo <hugolr@domain.rj.gov.br>
2023-01-23 10:18:15 -0300

    1:35:50

--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('api/products/', include('products.urls')) #poderia ter sido incluida nos urls da api ao invés de aqui 
 ]
